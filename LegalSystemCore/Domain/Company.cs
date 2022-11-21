1073531e8862105f949ef67cd24f7b203c146d51
JackHallwalker <epcravisanka@gmail.com>
2022-11-22 01:50:14 +0530

    Company DAO, Controller, Domain, DAO Factory updated

---
+++ LegalSystemCore/Domain/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LegalSystemCore
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string CompanyAddress { get; set; }
+    }
+}
