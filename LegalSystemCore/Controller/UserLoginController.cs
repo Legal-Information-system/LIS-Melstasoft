03c6817192576e9d07481f8a31194cfbb5381575
Charith Anjana <charithanjana01@gmail.com>
2022-11-22 21:53:27 +0530

    bug fixed

--- LegalSystemCore/Controller/UserLoginController.cs
+++ LegalSystemCore/Controller/UserLoginController.cs
@@ -10,81 +10,88 @@ using System.Text;
 
 namespace LegalSystemCore.Controller
 {
-    
-        public interface IUserLoginController
-        {
-            int Save(UserLogin  userLogin);
-            int Update(UserLogin userLogin);
-            List<UserLogin> GetUserLoginList();
 
+    public interface IUserLoginController
+    {
+        int Save(UserLogin userLogin);
+        int Update(UserLogin userLogin);
+        List<UserLogin> GetUserLoginList();
+
+    }
+
+    public class UserLoginControllerImpl : IUserLoginController
+    {
+        IUserLoginDAO userLoginDAO = DAOFactory.CreateUserLoginDAO();
+
+        public int Save(UserLogin userLogin)
+        {
+            Common.DbConnection dbconnection = null;
+            try
+            {
+                dbconnection = new Common.DbConnection();
+                return userLoginDAO.Save(userLogin, dbconnection);
+            }
+            catch (Exception)
+            {
+                dbconnection.RollBack();
+                throw;
+            }
+            finally
+            {
+                if (dbconnection.con.State == System.Data.ConnectionState.Open)
+                {
+                    dbconnection.Commit();
+                }
+            }
         }
 
-        public class UserLoginControllerImpl : IUserLoginController
+        public int Update(UserLogin userLogin)
         {
-            IUserLoginDAO userLoginDAO = DAOFactory.CreateUserLoginDAO();
-
-            public int Save(UserLogin userLogin)
+            Common.DbConnection dbconnection = null;
+            try
             {
-                Common.DbConnection dbconnection = null;
-                try
+                dbconnection = new Common.DbConnection();
+                return userLoginDAO.Update(userLogin, dbconnection);
+            }
+            catch (Exception)
+            {
+                dbconnection.RollBack();
+                throw;
+            }
+            finally
+            {
+                if (dbconnection.con.State == System.Data.ConnectionState.Open)
                 {
-                    dbconnection = new Common.DbConnection();
-                    return userLoginDAO.Save(userLogin,dbconnection);
-                }
-                catch (Exception)
-                {
-                    dbconnection.RollBack();
-                    throw;
-                }
-                finally
-                {
-                    if (dbconnection.con.State == System.Data.ConnectionState.Open)
-                    {
-                        dbconnection.Commit();
-                    }
+                    dbconnection.Commit();
                 }
             }
-
-            public int Update(UserLogin userLogin)
-            {
-                Common.DbConnection dbconnection = null;
-                try
-                {
-                    dbconnection = new Common.DbConnection();
-                    return userLoginDAO.Update(userLogin, dbconnection);
-                }
-                catch (Exception)
-                {
-                    dbconnection.RollBack();
-                    throw;
-                }
-            }
-
-            public List<UserLogin> GetUserLoginList()
-            {
-                Common.DbConnection dbConnection = null;
-                List<UserLogin> listUserLogin = new List<UserLogin>();
-                try
-                {
-                    dbConnection = new Common.DbConnection();
-                    listUserLogin = userLoginDAO.GetUserLoginList(dbConnection);
-                    
-                }
-                catch (Exception)
-                {
-                    dbConnection.RollBack();
-                    throw;
-                }
-                finally
-                {
-                    if (dbConnection.con.State == System.Data.ConnectionState.Open)
-                    {
-                        dbConnection.Commit();
-                    }
-                }
-                return listUserLogin;
-            }
-
         }
-    
+
+        public List<UserLogin> GetUserLoginList()
+        {
+            Common.DbConnection dbConnection = null;
+            List<UserLogin> listUserLogin = new List<UserLogin>();
+            try
+            {
+                dbConnection = new Common.DbConnection();
+                listUserLogin = userLoginDAO.GetUserLoginList(dbConnection);
+
+            }
+            catch (Exception)
+            {
+                dbConnection.RollBack();
+                throw;
+            }
+            finally
+            {
+                if (dbConnection.con.State == System.Data.ConnectionState.Open)
+                {
+                    dbConnection.Commit();
+                }
+            }
+            return listUserLogin;
+        }
+
+    }
+
 }
