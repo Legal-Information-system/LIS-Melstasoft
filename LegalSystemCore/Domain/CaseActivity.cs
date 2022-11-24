5f71be40437777e353293237cc1337f424bb6adf
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2022-11-24 13:09:33 +0530

    CaseActivity

---
+++ LegalSystemCore/Domain/CaseActivity.cs
@@ -0,0 +1,53 @@
+﻿using LegalSystemCore.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LegalSystemCore.Domain
+{
+    public class CaseActivity
+    {
+        [DBField("case_activit_id")]
+        public int CaseActivitId { get; set; }
+
+        [DBField("case_number")]
+        public string CaseNumber { get; set; }
+        [DBField("activity_date")]
+        public DateTime ActivityDate { get; set; }
+
+        [DBField("assign_attorney_id")]
+        public int AssignAttorneyId { get; set; }
+
+        [DBField("counsilor_id")]
+        public int CounsilorId { get; set; }
+        [DBField("other_side_lawyer")]
+        public string OtherSideLawyer { get; set; }
+
+        [DBField("judge_name")]
+        public string JudgeName { get; set; }
+
+        [DBField("company_rep")]
+        public string CompanyRep { get; set; }
+
+        [DBField("action_taken_id")]
+        public int ActionTakenId { get; set; }
+
+        [DBField("next_date")]
+        public DateTime NextDate { get; set; }
+
+
+        [DBField("remarks")]
+        public string Remarks { get; set; }
+
+        [DBField("next_action_id")]
+        public int NextActionId { get; set; }
+        [DBField("create_user_id")]
+        public int CreateUserId { get; set; }
+
+
+        [DBField("is_active")]
+        public int IsActive { get; set; }
+
+    }
+}
---
+++ LegalSystemCore/Infrastructure/CaseActivityDAO.cs
@@ -0,0 +1,65 @@
+﻿using LegalSystemCore.Common;
+using LegalSystemCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LegalSystemCore.Infrastructure
+{
+    public interface IUpdateCaseDAO
+    {
+        int Save(CaseActivity caseActivity, DbConnection dbConnection);
+        int Update(CaseActivity caseActivity, DbConnection dbConnection);
+        int Delete(CaseActivity caseActivityte, DbConnection dbConnection);
+        List<CaseActivity> GetCaseActivityList(DbConnection dbConnection);
+        CaseActivity GetActivityCase(string caseActivitys, DbConnection dbConnection);
+    }
+
+    public class UpdateCaseDAOSqlImpl : IUpdateCaseDAO
+    {
+        public CaseActivity GetActivityCase(string caseActivitys, DbConnection dbConnection)
+        {
+            CaseActivity caseActivity = new CaseActivity();
+
+            dbConnection = new DbConnection();
+
+            dbConnection.cmd.CommandText = "select * from case_activity WHERE case_number =" + caseActivitys + "AND is_active = 1";
+
+            dbConnection.dr = dbConnection.cmd.ExecuteReader();
+            DataAccessObject dataAccessObject = new DataAccessObject();
+            caseActivity = dataAccessObject.GetSingleOject<CaseActivity>(dbConnection.dr);
+            dbConnection.dr.Close();
+
+
+            return caseActivity;
+        }
+
+        public int Save(CaseActivity caseActivity, DbConnection dbConnection)
+        {
+            int output = 0;
+
+            dbConnection.cmd.Parameters.Clear();
+            dbConnection.cmd.CommandType = System.Data.CommandType.Text;
+            dbConnection.cmd.CommandText = "Insert into case_activity (case_number, activity_Date, assign_attorney_id,counsilor_id , other_side_lawyer, judge_name, company_rep, action_taken_id, next_date, remarks, " +
+                "next_action_id, create_user_id ) values (@CaseNumber,@ActivityDate,@AssignAttorneyId,@CounsilorId,@OtherSideLawyer,@JudgeName,@CompanyRep,@ActionTakenId,@ActionTakenId,@NextDate,@Remarks,@NextActionId,@CreateUserId)";
+
+            dbConnection.cmd.Parameters.AddWithValue("@CaseNumber", caseActivity.CaseNumber);
+            dbConnection.cmd.Parameters.AddWithValue("@ActivityDate", caseActivity.ActivityDate);
+            dbConnection.cmd.Parameters.AddWithValue("@AssignAttorneyId", caseActivity.AssignAttorneyId);
+            dbConnection.cmd.Parameters.AddWithValue("@CounsilorId", caseActivity.CounsilorId);
+            dbConnection.cmd.Parameters.AddWithValue("@OtherSideLawyer", caseActivity.OtherSideLawyer);
+            dbConnection.cmd.Parameters.AddWithValue("@JudgeName", caseActivity.JudgeName);
+            dbConnection.cmd.Parameters.AddWithValue("@CompanyRep", caseActivity.CompanyRep);
+            dbConnection.cmd.Parameters.AddWithValue("@ActionTakenId", caseActivity.ActionTakenId);
+            dbConnection.cmd.Parameters.AddWithValue("@NextDate", caseActivity.NextDate);
+            dbConnection.cmd.Parameters.AddWithValue("@Remarks", caseActivity.Remarks);
+            dbConnection.cmd.Parameters.AddWithValue("@NextActionId", caseActivity.NextActionId);
+            dbConnection.cmd.Parameters.AddWithValue("@CreateUserId", caseActivity.CreateUserId);
+
+            output = Convert.ToInt32(dbConnection.cmd.ExecuteScalar());
+
+            return output;
+        }
+    }
+}
