e73d7aeb50a75fffa6bfebabedcd97ac9ab30353
JackHallwalker <epcravisanka@gmail.com>
2023-02-13 10:04:31 +0530

    update

--- LegalSystemWeb/ViewCases.aspx.designer.cs
+++ LegalSystemWeb/ViewCases.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace LegalSystemWeb
         protected global::System.Web.UI.UpdatePanel UpdatePanel1;
 
         /// <summary>
+        /// txtSearch control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSearch;
+
+        /// <summary>
         /// ddlCaseStatus control.
         /// </summary>
         /// <remarks>
